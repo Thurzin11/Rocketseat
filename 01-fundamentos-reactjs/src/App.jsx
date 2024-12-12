115d4ee8b9daf8dbe62dace343b77e841912dfb6
Thurzin11 <arthursantosgoncalves587@gmail.com>
2024-12-12 08:29:14 -0300

    add comment is done

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -6,131 +6,57 @@ import Post from "./components/Post";
 function App() {
   const posts = [
     {
-      author: "Arthur Santos Gonçalves",
-      content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      id: 1,
+      author: {
+        avatarUrl: "https://github.com/Thurzin11.png",
+        name: "Thurzin",
+        role: "Software Developer",
+      },
+      content:
+        "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      publishedAt: new Date("2024-12-11 09:00:00"),
       imgPath: "https://picsum.photos/200/300?random=1",
       comments: [
-        { 
+        {
           author: "Jose da Silva",
           textComment: "Parabéns, ficou muito bom!",
-        }
-      ]
+        },
+      ],
     },
     {
-      author: "Maria Oliveira",
-      content: "Concluí o curso de JavaScript e já estou desenvolvendo meu primeiro projeto!",
-      imgPath: "https://picsum.photos/200/300?random=2",
+      id: 2,
+      author: {
+        avatarUrl: "https://github.com/Thurzin11.png",
+        name: "Jose da Silva",
+        role: "Software Developer",
+      },
+      content:
+        "Concluí o curso de JavaScript e já estou desenvolvendo meu primeiro projeto!",
+      publishedAt: new Date("2024-12-10 09:00:00"),
+      imgPath: "https://picsum.photos/200/300?random=1",
       comments: [
-        { 
-          author: "Carlos Alberto",
-          textComment: "Muito bom, Maria! Continue assim!",
-        }
-      ]
+        {
+          author: "Arthur",
+          textComment: "Parabéns, ficou muito bom!",
+        },
+      ],
     },
-    {
-      author: "João Pedro",
-      content: "Hoje foi meu primeiro dia na Premiersoft Academy. Estou muito animado!",
-      imgPath: "https://picsum.photos/200/300?random=3",
-      comments: [
-        { 
-          author: "Ana Beatriz",
-          textComment: "Parabéns pela conquista, João! Sucesso na sua jornada!",
-        }
-      ]
-    },
-    {
-      author: "Camila Santos",
-      content: "Desenvolvi uma aplicação de controle de tarefas usando React e TypeScript. Foi um grande aprendizado!",
-      imgPath: "https://picsum.photos/200/300?random=4",
-      comments: [
-        { 
-          author: "Felipe Costa",
-          textComment: "Ficou top, Camila! Eu também quero aprender React!",
-        }
-      ]
-    },
-    {
-      author: "Ricardo Almeida",
-      content: "Passei no processo seletivo para a empresa dos meus sonhos! Agora sou desenvolvedor pleno!",
-      imgPath: "https://picsum.photos/200/300?random=5",
-      comments: [
-        { 
-          author: "Luana Ferreira",
-          textComment: "Parabéns, Ricardo! Você merece muito essa conquista!",
-        }
-      ]
-    },
-    {
-      author: "Gabriela Souza",
-      content: "Finalizei meu portfólio online e já estou começando a divulgar para empresas!",
-      imgPath: "https://picsum.photos/200/300?random=6",
-      comments: [
-        { 
-          author: "Marcos Vinícius",
-          textComment: "Seu portfólio ficou incrível, Gabriela! Boa sorte na sua busca por oportunidades!",
-        }
-      ]
-    },
-    {
-      author: "Eduardo Lima",
-      content: "Consegui minha primeira vaga de estágio na área de desenvolvimento de software!",
-      imgPath: "https://picsum.photos/200/300?random=7",
-      comments: [
-        { 
-          author: "Carla Mendes",
-          textComment: "Parabéns, Eduardo! Muito feliz por você!",
-        }
-      ]
-    },
-    {
-      author: "Fernanda Ribeiro",
-      content: "Hoje consegui resolver um bug que estava me travando por 3 dias. A sensação de vitória é indescritível!",
-      imgPath: "https://picsum.photos/200/300?random=8",
-      comments: [
-        { 
-          author: "Rafael Nunes",
-          textComment: "Eu sei bem como é, Fernanda! Parabéns pela persistência!",
-        }
-      ]
-    },
-    {
-      author: "Gustavo Henrique",
-      content: "Acabei de lançar minha primeira extensão para o VS Code! Ela ajuda a formatar JSON de forma automática.",
-      imgPath: "https://picsum.photos/200/300?random=9",
-      comments: [
-        { 
-          author: "Pedro Martins",
-          textComment: "Que da hora, Gustavo! Vou instalar agora mesmo!",
-        }
-      ]
-    },
-    {
-      author: "Isabela Costa",
-      content: "Publiquei meu primeiro artigo técnico no Medium sobre Clean Code!",
-      imgPath: "https://picsum.photos/200/300?random=10",
-      comments: [
-        { 
-          author: "Leonardo Borges",
-          textComment: "Muito bom, Isabela! Já vou salvar para ler depois!",
-        }
-      ]
-    }
   ];
-  
-  
+
   return (
     <div>
       <Header />
       <div className={styles.wrapper}>
-        <Sidebar/>
+        <Sidebar />
         <main>
           {posts.map((post) => (
             <Post
-            key={post.author}
-              author={post.author}
+              key={post.id}
+              author={post.author.name}
               content={post.content}
               comments={post.comments}
-              imgPath={post.imgPath}
+              imgPath={post.author.avatarUrl}
+              postedAt={post.publishedAt}
             />
           ))}
         </main>
--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,37 +1,50 @@
 import PropTypes from "prop-types";
 import Comment from "./Comment";
 import styles from "./Post.module.css";
-
-
+import { format, formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { useState } from "react";
 
 export default function Post(props) {
+  const publisheDateFormater = format(props.postedAt,"dd 'de' LLLL 'de' yyyy 'as' HH:mm'h'",{
+    locale: ptBR,
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(props.postedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+  const [comments, setComments] = useState(props.comments);
+
+  const handleCommentSubmit = (event) => {
+    event.preventDefault();
+    const textComment = event.target.comment.value;
+    const newComment = {
+      author: "Arthur",
+      textComment,
+    }  
+    setComments([...comments,newComment])
+  }
+
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src={props.imgPath}
-            alt=""
-          />
+          <img className={styles.avatar} src={props.imgPath} alt="" />
           <div className={styles.authorInfo}>
             <strong>{props.author}</strong>
             <span>Web Developer</span>
           </div>
         </div>
 
-        <time title="10 de dezembro de 2024 as 13h" dateTime="2024-10-12">
-          Publicado a 1h
+        <time title={publisheDateFormater} dateTime={props.postedAt.toISOString()}>
+          {publishedDateRelativeToNow} atras
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-
-        <p>
-          {props.content}
-        </p>
-
+        <p>{props.content}</p>
         <p>
           👉 <a href="">jane.design/doctorcare</a>
         </p>
@@ -42,23 +55,21 @@ export default function Post(props) {
           <a href="">#rocketseat</a>
         </p>
       </div>
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCommentSubmit} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
-        <textarea placeholder="Deixe um comentário" />
+        <textarea placeholder="Deixe um comentário" name="comment" />
         <footer>
           <button type="submit">Comentario</button>
         </footer>
       </form>
       <div className={styles.commentList}>
-        {
-          props.comments.map((comment, index) => (
-            <Comment
-              key={index}
-              author={comment.author}
-              textCommand={comment.textComment}
-            />
-          ))
-        }
+        {comments.map((comment, index) => (
+          <Comment
+            key={index}
+            author={comment.author}
+            textCommand={comment.textComment}
+          />
+        ))}
       </div>
     </article>
   );
@@ -68,6 +79,7 @@ Post.propTypes = {
   author: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
   imgPath: PropTypes.string.isRequired,
+  postedAt: PropTypes.any.isRequired,
   comments: PropTypes.arrayOf(
     PropTypes.shape({
       author: PropTypes.string.isRequired,
